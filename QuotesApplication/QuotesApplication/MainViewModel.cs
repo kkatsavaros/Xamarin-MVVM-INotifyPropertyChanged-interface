f60c427e29ad20decae9ddbc0edb29ad7e6dfa44
kkats <kkats@DESKTOP-9HAEMB4>
2022-12-14 12:24:52 +0200

    Add project files.

---
+++ QuotesApplication/QuotesApplication/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Windows.Input;
+using Xamarin.Forms;
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace QuotesApplication
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+
+
+        private string _userName;   //propfull + Tab.
+
+        public string UserName      // 'Οτι γράφω στο Entry πηγαίνει στο property UserName.
+        {
+            get { return _userName; }
+            set { _userName = value; OnPropertyChanged("UserName"); }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,12 @@
+Xamarin MVVM INotifyPropertyChanged interface
+
+
+With that small program i show how to use INotifyPropertyChanged interface.
+
+With that interface there is no need to refresh the page.
+
+
+
+
+Katsavaros Konstantinos
+
